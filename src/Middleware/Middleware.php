043d538a2df5dc1a503fc16e834a78a7b32d2d9c
Francis Carbah <fcarbah@gmail.com>
2019-08-18 22:52:56 -0400

    added base middleware class

---
+++ src/Middleware/Middleware.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Feather\Middleware;
+use Feather\Http\Request;
+use Feather\Http\Response;
+use Feather\Http\Input;
+/**
+ * Description of Middleware
+ *
+ * @author fcarbah
+ */
+abstract class Middleware {
+    
+    protected $request;
+    protected $response;
+    protected $errorCode;
+    protected $errorMessage;
+    protected $rediretUri = '/';
+    protected $input;
+    
+    public function __construct() {
+        $this->input = Input::getInstance();
+        $this->request = Request::getInstance();
+        $this->response = Response::getInstance();
+        $this->errorCode = 0;// 0 means no error
+        $this->errorMessage='';
+    }
+    
+    abstract function run();
+    
+    public function errorCode(){
+        return $this->errorCode;
+    }
+    
+    public function errorMessage(){
+        return $this->errorMessage;
+    }
+    
+    public function redirect(){
+        
+        ob_flush();
+        
+        $res = \Feather\Objects\Response::error($this->errorMessage);
+        
+        if($this->request->isAjax){
+            return $this->response->renderJSON($res->toArray(), [], $this->errorCode);
+        }
+        
+        else{
+            \Feather\Http\Session::save(['data'=>$res->toArray()], REDIRECT_DATA_KEY);
+            return $this->response->redirect($this->rediretUri);
+        }
+        
+        die;
+        
+    }
+    
+}
