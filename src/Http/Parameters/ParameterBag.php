439e1817adc9bd73b01625781a5eb97fa92640d8
Francis Carbah <fcarbah@gmail.com>
2020-12-26 10:05:46 -0500

    fixed bug in parameter bag

--- src/Http/Parameters/ParameterBag.php
+++ src/Http/Parameters/ParameterBag.php
@@ -9,41 +9,41 @@ namespace Feather\Init\Http\Parameters;
  */
 class ParameterBag implements \Iterator, \ArrayAccess, \JsonSerializable
 {
-    /** @var array **/
-    private $_items = array();
-    
+
+    /** @var array * */
+    protected $_items = array();
+
     /**
-     * 
+     *
      * @param \stdClass|array $input
      */
     public function __construct($input = array())
     {
-        if(is_object($input)){
+        if (is_object($input)) {
             $this->setObject($input);
-        }else if(is_array($input)){
+        } else if (is_array($input)) {
             $this->_items = $input;
-        }else{
+        } else {
             $this->_items[] = $input;
         }
-        
     }
-    
+
     /**
-     * 
+     *
      * @param string $name
      * @return mixed
      */
     public function __get($name)
     {
-        if(isset($this->_items[$name])){
+        if (isset($this->_items[$name])) {
             return $this->_items[$name];
         }
-        
+
         return null;
     }
-    
+
     /**
-     * 
+     *
      * @param string $name
      * @param mixed $value
      */
@@ -51,43 +51,43 @@ class ParameterBag implements \Iterator, \ArrayAccess, \JsonSerializable
     {
         $this->_items[$name] = $value;
     }
-    
+
     /**
-     * 
+     *
      * @param \stdClass|array $input
      * @throws ParameterBagException
      * @return \Feather\Init\Http\Parameters\ParameterBag
      */
-    public function addItems($input){
-        
-        if(is_array($input)){
-            $this->_items = array_merge($this->_items,$input);
-        }
-        else if (is_array($input)){
+    public function addItems($input)
+    {
+
+        if (is_array($input)) {
+            $this->_items = array_merge($this->_items, $input);
+        } else if (is_object($input)) {
             $this->setObject($input);
-        }
-        else{
+        } else {
             throw new ParameterBagException("Invalid input. Input is not an array or object");
         }
-        
+
         return $this;
-        
     }
-    
+
     /**
-     * 
+     *
      * @return array
      */
-    public function bag(){
+    public function bag()
+    {
         return $this->_items;
     }
-    
+
     /**
      * Get boolean value of parameter name
      * @param string $name
      * @return boolean
      */
-    public function boolean($name){
+    public function boolean($name)
+    {
         return boolval($this->{$name});
     }
 
@@ -95,61 +95,68 @@ class ParameterBag implements \Iterator, \ArrayAccess, \JsonSerializable
     {
         return current($this->_items);
     }
-    
+
     /**
      * Get float value of parameter name
      * @param type $name
      * @return float
      */
-    public function float($name){
+    public function float($name)
+    {
         return floatval($this->{$name});
     }
+
     /**
      * Get integer value of parameter name
      * @param string $name
      * @param int $base
      * @return int
      */
-    public function int($name,$base = 10){
+    public function int($name, $base = 10)
+    {
         return intval($this->{$name}, $base);
     }
-    
+
     public function key()
     {
         return key($this->_items);
     }
+
     /**
-     * 
+     *
      * @param \Feather\Init\Http\Parameters\ParameterBag $bag
      * @return \Feather\Init\Http\Parameters\ParameterBag
      */
-    public function merge(ParameterBag $bag){
-        $this->_items = array_merge($this->_items,$bag->bag());
+    public function merge(ParameterBag $bag)
+    {
+        $this->_items = array_merge($this->_items, $bag->bag());
         return $this;
     }
-    
+
     /**
      * Get string value of parameter name
      * @param string $name
      * @return string
      */
-    public function string($name){
+    public function string($name)
+    {
         return strval($this->{$name});
     }
-    
+
     /**
-     * 
+     *
      * @return string
      */
-    public function toString(){
-        $string= '';
-        foreach($this->_items as $key=>$val){
-            $string .= $key.'='.$val.'&';
+    public function toString()
+    {
+        $string = '';
+        foreach ($this->_items as $key => $val) {
+            $string .= $key . '=' . $val . '&';
         }
-        
-        return substr($string, 0,-1);
+
+        return substr($string, 0, -1);
     }
-    
+
     public function next(): void
     {
         next($this->_items);
@@ -165,17 +172,17 @@ class ParameterBag implements \Iterator, \ArrayAccess, \JsonSerializable
         $key = key($this->_items);
         return $key !== null && $key !== false;
     }
-    
+
     /**
-     * 
+     *
      * @param \stdClass $object
      */
-    protected function setObject($object){
-        
-        foreach(get_object_vars($object) as $key=>$value){
-            $this->_items[$key] = $value;            
+    protected function setObject($object)
+    {
+
+        foreach (get_object_vars($object) as $key => $value) {
+            $this->_items[$key] = $value;
         }
-        
     }
 
     public function offsetExists($offset): bool
@@ -195,7 +202,7 @@ class ParameterBag implements \Iterator, \ArrayAccess, \JsonSerializable
 
     public function offsetUnset($offset): void
     {
-        if($this->offsetExists($offset)){
+        if ($this->offsetExists($offset)) {
             unset($this->_items[$offset]);
         }
     }
